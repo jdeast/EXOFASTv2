59f47702740f150a99bf89afc0fa6572733b2ac9
Jason Eastman (windows) <jdeast@gmail.com>
2023-06-09 12:43:07 -0400

    Update setup.bat
    tweak setup.bat for windows users

--- setup.bat
+++ setup.bat
@@ -1,27 +1,27 @@
-setx MODELDIR %HOME%\modeling\
-setx EXOFAST_PATH %HOME%\idl\EXOFASTv2
-setx IDL_PATH "<IDL_DEFAULT>:+$HOME/idl"
+@setx MODELDIR %HOME%\modeling\
+@setx EXOFAST_PATH %HOME%\idl\EXOFASTv2
+@setx IDL_PATH "<IDL_DEFAULT>:+$HOME/idl"
 
 :: This is hardcoded to 64 bit IDL v8.8. Change to your actual path and uncomment before executing
-setx PATH "%PATH%;%PROGRAMFILES%\Harris\IDL88\bin\bin.x86_64\"
+@setx PATH "%PROGRAMFILES%\Harris\IDL88\bin\bin.x86_64\"
 
-cd %HOME%\idl
+@cd %HOME%\idl
 if exist EXOFASTv2\ (
   echo "EXOFASTv2 already installed"
 ) else (
-  git clone https://github.com/jdeast/EXOFASTv2.git
+  @git clone https://github.com/jdeast/EXOFASTv2.git
 )
 
 if exist IDLAstro\ (
   echo "IDLAstro already installed"
 ) else (
-  git clone https://github.com/wlandsman/IDLAstro.git
+  @git clone https://github.com/wlandsman/IDLAstro.git
 )
 
 if exist coyote\ (
   echo "coyote already installed"
 ) else (
-  git clone https://github.com/idl-coyote/coyote.git
+  @git clone https://github.com/idl-coyote/coyote.git
 )
 
-%EXOFAST_PATH%\examples\hat3_nolicense\fithat3.bat
+@%EXOFAST_PATH%\examples\hat3_nolicense\fithat3.bat
