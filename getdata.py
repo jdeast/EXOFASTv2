d58b080c824f8ceb342cbf58f8b88213d8426fa7
jdeast <jdeast@gmail.com>
2023-08-23 11:54:37 -0400

    now takes any id; gets data from Kepler or TESS

--- getdata.py
+++ getdata.py
@@ -2,7 +2,7 @@ import lightkurve as lk
 import numpy as np
 import sys
 import datetime
-import ipdb
+#import ipdb
 
 # this uses lightkurve to extract QLP/SPOC TESS data and format it for EXOFASTv2
 # call it like this (python 3)
@@ -33,8 +33,13 @@ import ipdb
 t0 = datetime.datetime(2000,1,1)
 jd0 = 2451544.5
 
-ticid = 'TIC ' + str(sys.argv[1])
-search_result = lk.search_lightcurve(ticid,mission='TESS',author=('SPOC','QLP'))
+#ticid = 'TIC ' + str(sys.argv[1])
+id = str(sys.argv[1])
+search_result = lk.search_lightcurve(id)#,mission='TESS',author=('SPOC','QLP'))
+
+if len(search_result) == 0:
+    print("No light curves found for " + id + ". Try a different name.")
+    sys.exit()
 
 lcs = search_result.download_all()
 i=0
@@ -44,15 +49,32 @@ for lc in lcs:
 #    lc = lc.remove_outliers() # this often clips the transits
 #    lc = lc.flatten() # this can remove transit signals
     lc = lc.normalize()
+
+    author = search_result[i].author[0] # SPOC, QLP, etc
+    exptime = str(int(search_result[i].exptime[0].value)).zfill(4)
+
+    if author == "Kepler":
+        bjd_offset = 2454833.0
+        sector = 'Q' + search_result[i].mission[0][-2:]
+        filter = 'Kepler'
+        telescope = 'Kepler'
+    elif author == "TESS-SPOC" or author=='QLP' or author=='SPOC':
+        bjd_offset = 2457000.0
+        sector = 'S' + str(lc.sector).zfill(2)
+        filter = 'TESS'
+        telescope = 'TESS'
+    else:
+        print("WARNING: Skipping lightcurve with unrecognized author: " + author)
+        continue
     
-    time = np.array(lc.time.value) + 2457000.0
+    time = np.array(lc.time.value) + bjd_offset
     flux = np.array(lc.flux.value)
     err = np.array(lc.flux_err.value)
 
     datestr = (t0 + datetime.timedelta(days=time[0]-jd0)).strftime('n%Y%m%d')
 
     # create the filename in EXOFASTv2 format
-    filename = datestr + '.TESS.TESS.' + 'TIC' + str(search_result[i].target_name.data[0]) + '.S' + str(lc.sector).zfill(2) + '.' + str(int(search_result[i].exptime[0].value)).zfill(4) + '.' + lc.author + '.dat' # SPOC, QLP, etc
+    filename = datestr + '.' + filter + '.' + telescope + '.' + id + '.' + sector + '.' + exptime + '.' + author + '.dat' 
 
     np.savetxt(filename,np.column_stack([time,flux,err]))
 
